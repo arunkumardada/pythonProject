36345d108910236a9f21d8f48c93837024e830ff
Arun Kumar <cdarun.chinna@gmail.com>
2022-10-27 00:17:26 +0530

    initial commit
    updated readme and mainpy file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pythonAssignmentUMLDigitalHealthcourse
- 
+
+Environment Used : PyCharm
+
+Steps:
+
+1. Download Pycharm Editor
+2. Install python
+3. Open pycharm and Clone the repo of this project
+4. Run main.py (RightClick main.py and Run 'main' )
+5. Two Files mytextfile.txt and mytextfile2 output result will be displayed.
--- main.py
+++ main.py
@@ -8,6 +8,38 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+    file1 = open('mytextfile.txt', 'w')
+    file2 = open('mytextfile2.txt', 'w')
+
+    X = [] * 10000
+    Y = []
+    Z = []
+
+    for i in range(1, 10001, 1):
+        X.append(i)
+    print(X)
+
+    for number in range(1, 10000, 1):
+        if number > 1:
+            for i in range(2, number):
+                if (number % i) == 0:
+                    break
+            else:
+                Y.append(number)
+                file1.write(str(number) + '\n')
+
+    print(Y)
+    file1.close()
+
+    for index, value in enumerate(Y, 1):
+        if index in Y:
+            continue
+        else:
+            Z.append(Y[index - 1])
+            file2.write(str(Y[index - 1]) + '\n')
+    print(Z)
+    file2.close()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
